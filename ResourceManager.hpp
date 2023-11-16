43ae2010780a79470783354b0553c77494292b24
killpopmusic <tomek.talarczyk@gmail.com>
2023-11-16 21:44:31 +0100

    new manager

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,19 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    
+    public:
+    ResourceManager(): res(new Resource){};
+
+    ResourceManager(const ResourceManager& resM) : res(new Resource(*resM.res)) {};
+
+    ~ResourceManager(){
+        delete res;
+    }
+    private:
+    Resource*res;
+
+    
+
+
 };
